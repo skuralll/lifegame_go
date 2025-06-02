f6acc06f49bd900e35f7973a0347e16e58812635
shi-fujiwara <fuji.ktbl@gmail.com>
2025-06-02 17:14:29 +0900

    サーバーのメイン処理を実装し、ルーティングを登録する機能を追加。

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com.skuralll/lifegame_go/internal/routes"
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
-	println("Hello from server/main.go")
+	// Echo instance
+	e := echo.New()
+	// Register routes
+	routes.RegisterRoutes(e)
+	// Start Server
+	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start server", "error", err)
+	}
 }
---
+++ internal/routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com.skuralll/lifegame_go/internal/handler"
+	"github.com/labstack/echo/v4"
+)
+
+// エンドポイント登録
+func RegisterRoutes(e *echo.Echo) {
+	e.GET("hello", handler.SayHello)
+}
