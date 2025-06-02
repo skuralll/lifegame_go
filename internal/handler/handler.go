f06a5b89e1f64fb9325ba152076fc40b6a483410
skuralll <f20154@e.hiroshima-cu.ac.jp>
2025-06-02 22:35:37 +0900

    次世代の盤面情報を取得するハンドラを追加し、ルーティングを更新

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,10 +3,21 @@ package handler
 import (
 	"net/http"
 
+	"github.com.skuralll/lifegame_go/internal/game"
 	"github.com/labstack/echo/v4"
 )
 
 // サンプルハンドラ
 func SayHello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+// 次世代の盤面情報を取得するハンドラ
+func GetNextBoard(c echo.Context) error {
+	var board game.Board
+	if err := c.Bind(&board); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid board data"})
+	}
+
+	return c.JSON(http.StatusOK, board.GetNext());
 }
